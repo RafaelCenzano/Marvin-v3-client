e9ff2ebf127d0afa518ac961d44d25a4b31f267c
Savagecoder77 <savagecoder77@gmail.com>
2018-10-25 21:17:26 -0700

    add redirects and static path

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, make_response, redirect
 Marvin UI created with flask
 '''
 
-app = Flask(__name__, template_folder='templates')# declare flask app and point to html files for ui
+app = Flask(__name__, template_folder='templates', static_folder='static')# declare flask app and point to html files for ui
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -14,9 +14,12 @@ def page_not_found(e):
 def not_found():
     return render_template('not_found.html')
 
-@app.route('/', methods=['GET']) # home route
-def index():
-    return redirect("/dashboard", code=302) # redirect to dashboard
+@app.route('/', methods=['GET']) # dashboard route
+@app.route('/home', methods=['GET']) # dashboard route
+@app.route('/marvin', methods=['GET']) # dashboard route
+@app.route('/dashboards', methods=['GET']) # dashboard route
+def dashboardRedirect():
+    return redirect("/dashboard", code=302) # redirect to /404_not_found
 
 @app.route('/dashboard', methods=['GET']) # dashboard route
 def dashboard():
