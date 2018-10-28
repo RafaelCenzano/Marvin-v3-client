cf979b19e8830ef74653bd6f59ba848f8a3d346b
Savagecoder77 <savagecoder77@gmail.com>
2018-10-28 11:02:07 -0700

    reorganize code

--- app.py
+++ app.py
@@ -6,14 +6,12 @@ Marvin UI created with flask
 
 app = Flask(__name__, template_folder='templates', static_folder='static')# declare flask app and point to html files for ui
 
+# Error handlers
 @app.errorhandler(404)
 def page_not_found(e):
     return redirect("/404_not_found", code=302) # redirect to /404_not_found
 
-@app.route('/404_not_found', methods=['GET'])
-def not_found():
-    return render_template('not_found.html')
-
+# Redirects to main routes
 @app.route('/', methods=['GET']) # dashboard route
 @app.route('/home', methods=['GET']) # dashboard route
 @app.route('/marvin', methods=['GET']) # dashboard route
@@ -21,9 +19,21 @@ def not_found():
 def dashboardRedirect():
     return redirect("/dashboard", code=302) # redirect to /dashboard
 
+# Main Routes
+
 @app.route('/dashboard', methods=['GET']) # dashboard route
 def dashboard():
     return render_template('dashboard.html') # render dashboard.html for UI
+
+@app.route('/sendemail', methods=['GET']) # dashboard route
+def dashboard():
+        return render_template('dashboard.html') # render dashboard.html for UI
+
+# Misc Routes
+
+@app.route('/404_not_found', methods=['GET'])
+def not_found():
+    return render_template('not_found.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
