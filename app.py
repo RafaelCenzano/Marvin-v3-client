d880fd5984737ac4a8b0c237853474043d3deca3
Savagecoder77 <savagecoder77@gmail.com>
2018-10-29 09:53:15 -0700

    add new code to incorporate new self made marvin-env soon to come

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, make_response, redirect
+import sys
+sys.path.insert(0, './marvin-env/site-packages') # make env sitepackages folder in path for pip installed libraries
+from flask import Flask, render_template, request, make_response, redirect, request
 
 '''
 Marvin UI created with flask
@@ -38,6 +40,14 @@ def sendemail():
 @app.route('/404_not_found', methods=['GET'])
 def not_found():
     return render_template('not_found.html')
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+    return 'Server shutting down...'
 
 if __name__ == '__main__':
     app.run(debug=True)
