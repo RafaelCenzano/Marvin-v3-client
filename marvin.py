0c605c509725f68bc7db225c5c7400074e8c2862
Savagecoder77 <savagecoder77@gmail.com>
2018-10-29 17:46:12 -0700

    start marvin.py

---
+++ marvin.py
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0, './marvin-env/site-packages') # make env sitepackages folder in path for pip installed libraries
+sys.path.insert(0, './marvin/') # make env sitepackages folder in path for pip installed libraries
