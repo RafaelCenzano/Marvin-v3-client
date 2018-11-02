5053f38f01264357c36f3d34c28eb13687c225bc
Savagecoder77 <savagecoder77@gmail.com>
2018-11-02 08:00:34 -0700

    start new clean up process

--- setup.py
+++ setup.py
@@ -30,5 +30,16 @@ if __name__ == '__main__' and path.isfile('app.py') and path.isfile('requirement
     except Exception as e:
         print('We ran into a problem\nPlease report this issue ' + str(e) + '\nFiles couldn\'t be installed properly')
 
+    uneeded_dirs = ['word2number-1.1.dist-info','beautifulsoup4-4.6.3.dist-info','bs4-0.0.1.dist-info',
+                    'certifi-2018.10.15.dist-info','chardet-3.0.4.dist-info','Click-7.0.dist-info',
+                    'Flask-1.0.2.dist-info','gTTS_token-1.1.2.dist-info','gTTS-2.0.1.dist-info',
+                    'idna-2.7.dist-info','itsdangerous-1.1.0.dist-info','Jinja2-2.10.dist-info',
+                    'MarkupSafe-1.0.dist-info','playsound-1.2.2.dist-info','PyAudio-0.2.11.dist-info',
+                    'requests-2.20.0.dist-info','six-1.11.0.dist-info','SpeechRecognition-3.8.1.dist-info',
+                    'urllib3-1.24.dist-info','Werkzeug-0.14.1.dist-info','word2number-1.1.dist-info']
+
+    for dirs in uneeded_dirs:
+        pass
+
 else:
     print('Make sure to run this script in the Marvin-v3-client folder/directory')
