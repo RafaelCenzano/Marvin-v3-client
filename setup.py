26786698980d299ee3ef0d2178f17c0ba1634e6b
Savagecoder77 <savagecoder77@gmail.com>
2018-11-02 20:29:24 -0700

    remove uneeded files and folders with setup.py

--- setup.py
+++ setup.py
@@ -1,53 +1,91 @@
 #Imports
-from os import path, mkdir
+from os import path, mkdir, listdir, remove
 from os import system as terminal
-import json
+from json import dump
 from platform import system
+from shutil import rmtree
 
-if __name__ == '__main__' and path.isfile('app.py') and path.isfile('requirements.txt') and path.isfile('marvin-env/site-packages/__init__.py'):
+# Run code if file directly called and app.py, requirements.txt, and __init__.py in marvin-env exists to make many checks that setup.py called in right directory
+if __name__ == '__main__' and path.isfile('app.py') and path.isfile('requirements.txt') and path.isfile(path.join('marvin-env','lib','site-packages','__init__.py')):
 
     try:
+        # Make marvin-data directory to store any json and data needed to run marvin
+        mkdir('marvin-data')
+        print('\nmarvin-data folder created for marvin data\n')
 
-        try:
-            mkdir('marvin-data')
-        except FileExistsError:
-            print('Error marvin-data folder exists already might interfere with marvin data')
+        # If folder exsists prompt user that this can potentially interfere with marvin code
+    except FileExistsError:
+        print('Error marvin-data folder exists already might interfere with marvin data')
 
-        contacts_path = path.join('data','contacts.json')
-        settings_path = path.join('data','settings.json')
+    try:
+        # create any os paths for json files
+        contacts_path = path.join('marvin-data','contacts.json')
+        settings_path = path.join('marvin-data','settings.json')
+
+        # create json data for the json files
         contact_data = {"contacts":{},"nicks":{}}
         settings_data = {"settings":{"voice":"female","UI":"enabled"}}
 
-        with open(settings_path, 'w') as newfile:
-            json.dump(settings_data, newfile)
-        with open(contacts_path, 'w') as newfile1:
-            json.dump(contact_data, newfile1)
+        # create json files
+        with open(settings_path, 'w') as newjsonfile:
+            dump(settings_data, newjsonfile)
+        print('\nCreate settings.json files')
 
+        with open(contacts_path, 'w') as newjsonfile1:
+            dump(contact_data, newjsonfile1)
+        print('Create contacts.json files\n')
+
+        # If there is an error when creating data files
     except Exception as e:
         print('We ran into a problem\nPlease report this issue ' + str(e) + '\nFiles couldn\'t be created properly')
 
     try:
-        terminal('pip install -r requirements.txt -t marvin-env/site-packages/ --upgrade')
+        # install required libraries into marvin-env
+        print('\nInstalling needed libraries\n')
+        terminal('pip install -r requirements.txt -t ' + path.join('marvin-env','lib','site-packages') + ' --upgrade')
+
+        # dependencies couldn't be installed
     except Exception as e:
         print('We ran into a problem\nPlease report this issue ' + str(e) + '\nFiles couldn\'t be installed properly')
 
+        # make list full of uneeded folders created from pip installs
     uneeded_dirs = ['word2number-1.1.dist-info','beautifulsoup4-4.6.3.dist-info','bs4-0.0.1.dist-info',
                     'certifi-2018.10.15.dist-info','chardet-3.0.4.dist-info','Click-7.0.dist-info',
                     'Flask-1.0.2.dist-info','gTTS_token-1.1.2.dist-info','gTTS-2.0.1.dist-info',
                     'idna-2.7.dist-info','itsdangerous-1.1.0.dist-info','Jinja2-2.10.dist-info',
                     'MarkupSafe-1.0.dist-info','playsound-1.2.2.dist-info','PyAudio-0.2.11.dist-info',
                     'requests-2.20.0.dist-info','six-1.11.0.dist-info','SpeechRecognition-3.8.1.dist-info',
-                    'urllib3-1.24.dist-info','Werkzeug-0.14.1.dist-info','word2number-1.1.dist-info']
+                    'urllib3-1.24.1.dist-info','Werkzeug-0.14.1.dist-info']
     try:
-        if system() == 'Windows':
-            remove_command = 'rmdir'
-        else:
-            remove_command = 'rm -r'
+        # for all uneeded folders delete them
+        print('\nDeleting uneeded folders\n')
         for dirs in uneeded_dirs:
-            mydir = path.join('marvin-env','lib','site-packages',dirs) # Format Paths
-            terminal(remove_command + mydir)
+
+            # create path to folder to delete
+            folder = path.join('marvin-env','lib','site-packages',dirs)
+
+            # remove all files in the directory
+            for files in listdir(folder):
+
+                # file path inside folder
+                file_path = path.join(folder, files)
+
+                # if file exists delete it
+                if path.isfile(file_path):
+                    remove(file_path)
+
+                # if dir exists delete it
+                elif path.isdir(file_path):
+                    rmtree(file_path)
+
+            # finally delete folder
+            rmtree(folder)
+            print('deleted ' + folder + ' folder')
+
+        # files and folders couln't be deleted
     except Exception as e:
         print('This error occured when trying to remove uneeded folders to save space' + str(e) + '. If this a problem please report the error to the github repository')
 
+    # file ran from outside marvin folder which causes path problems for file creation
 else:
     print('Make sure to run this script in the Marvin-v3-client folder/directory')
