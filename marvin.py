195b5b6643c1558b2f57e8376872c7cf987b0e0f
Savagecoder77 <savagecoder77@gmail.com>
2018-11-04 20:33:56 -0800

    change sys path

--- marvin.py
+++ marvin.py
@@ -1,3 +1,4 @@
 import sys
-sys.path.insert(0, './marvin-env/site-packages') # make env sitepackages folder in path for pip installed libraries
-sys.path.insert(0, './marvin/') # make env sitepackages folder in path for pip installed libraries
+from os import path
+sys.path.insert(0, path.join('marvin-env','lib','site-packages')) # make env sitepackages folder in path for pip installed libraries
+sys.path.insert(0, path.join('marvin')) # make env sitepackages folder in path for pip installed libraries
