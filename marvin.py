568dd7b8e89b892b5ed4aed86117c2058653e455
Savagecoder77 <savagecoder77@gmail.com>
2018-11-04 20:35:02 -0800

    renamed folder

--- marvin.py
+++ marvin.py
@@ -1,4 +1,4 @@
 import sys
 from os import path
 sys.path.insert(0, path.join('marvin-env','lib','site-packages')) # make env sitepackages folder in path for pip installed libraries
-sys.path.insert(0, path.join('marvin')) # make env sitepackages folder in path for pip installed libraries
+sys.path.insert(0, path.join('marvin-modules')) # make env sitepackages folder in path for pip installed libraries
