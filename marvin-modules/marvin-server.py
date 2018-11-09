66e1e2ec94bb42ebd6b8116ca7677c3bbfdffe02
Savagecoder77 <savagecoder77@gmail.com>
2018-11-08 20:56:18 -0800

    new file to hold code for interactions with marvin server

---
+++ marvin-modules/marvin-server.py
@@ -0,0 +1,4 @@
+import requests
+import json
+
+r = requests.post(' heroku thing login', data=json.dumps({'email':'test@example.com', 'password':'test123'}), headers={'content-type': 'application/json'})
