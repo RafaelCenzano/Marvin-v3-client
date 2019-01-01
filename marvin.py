f02825e2974ab4ac4c874fe26d61b1c88c6bd431
Savagecoder77 <savagecoder77@gmail.com>
2019-01-01 13:53:31 -0800

    add import of new file

--- marvin.py
+++ marvin.py
@@ -2,3 +2,5 @@ import sys
 from os import path
 sys.path.insert(0, path.join('marvin-env','lib','site-packages')) # make env sitepackages folder in path for pip installed libraries
 sys.path.insert(0, path.join('marvin-modules')) # make env sitepackages folder in path for pip installed libraries
+
+import gui
