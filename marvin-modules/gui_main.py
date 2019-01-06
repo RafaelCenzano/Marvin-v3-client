40234dbdfad051976b0fc6376a9f71397d83b665
Savagecoder77 <savagecoder77@gmail.com>
2019-01-06 12:24:01 -0800

    gui wrapper to hold together frames and pages

---
+++ marvin-modules/gui_main.py
@@ -0,0 +1,42 @@
+try:
+    # for Python2
+    import Tkinter as tk
+except ImportError:
+    # for Python3
+    import tkinter as tk
+
+from gui_pages import *
+
+LARGE_FONT = ("Verdana", 30)
+
+class MarvinGuiMain(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        # Initialize TK
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        # Create container
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand="True")
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, PageOne):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
