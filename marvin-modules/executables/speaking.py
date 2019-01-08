9e0a94631cdde7a4e29e80508f78d9fec1b46e18
Savagecoder77 <savagecoder77@gmail.com>
2019-01-07 20:58:09 -0800

    create excutable pyttsx3 code for removing problem when threading

--- marvin-modules/executables/speaking.py
+++ marvin-modules/executables/speaking.py
@@ -2,4 +2,6 @@
 import sys
 from os import path
 sys.path.insert(0, path.join(path.dirname(path.abspath(__file__)), 'marvin-modules')) # make env marvin-modules folder in path for pip installed libraries
-import speak
+import speak as marvin_speak
+
+marvin_speak.say(str(sys.argv[1])) # speak
