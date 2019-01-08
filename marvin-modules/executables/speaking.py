39278d1eb8e8b06de75ffca88da91d0afb585b61
Savagecoder77 <savagecoder77@gmail.com>
2019-01-07 20:50:54 -0800

    speaking executable

---
+++ marvin-modules/executables/speaking.py
@@ -0,0 +1,5 @@
+# Speak file
+import sys
+from os import path
+sys.path.insert(0, path.join(path.dirname(path.abspath(__file__)), 'marvin-modules')) # make env marvin-modules folder in path for pip installed libraries
+import speak
