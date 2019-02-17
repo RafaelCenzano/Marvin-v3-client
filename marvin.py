aec48c5a21d767959f854ea35470cd398378c6d8
Savagecoder77 <savagecoder77@gmail.com>
2019-02-16 17:44:10 -0800

    change import method

--- marvin.py
+++ marvin.py
@@ -1,9 +1,8 @@
 import sys
 from os import path
 sys.path.insert(0, path.join('marvin-env','lib','site-packages')) # make env sitepackages folder in path for pip installed libraries
-sys.path.insert(0, path.join('marvin-modules')) # make env marvin-modules folder in path for pip installed libraries
-sys.path.insert(0, path.join('marvin-modules','gui')) # make env marvin-modules folder in path for pip installed libraries
-from gui_manager import gui
+
+from marvin-modules.gui.gui_manager import gui
 
 gui.mainloop()
 
