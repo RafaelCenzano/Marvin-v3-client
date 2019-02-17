a54d384c76a99b2fc26d11d319c9007bd042d189
Savagecoder77 <savagecoder77@gmail.com>
2019-02-16 17:44:23 -0800

    change and add code for imports

--- marvin-modules/pyttsx_speak.py
+++ marvin-modules/pyttsx_speak.py
@@ -1,7 +1,12 @@
+# env imports
+import sys
+from os import path
 sys.path.insert(0, path.join('marvin-env','lib','site-packages')) # make env sitepackages folder in path for pip installed libraries
+
+# marvin imports
 import pyttsx3 # run pyttsx3
 
-class Pyttsx3Speak():
+class Pyttsx3Speak:
 
     def __init__(self): # initialize engine function
         self.engine = pyttsx3.init() # initialize engine
