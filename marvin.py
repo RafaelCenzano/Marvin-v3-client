6f2f90d11e0220a97cd862df94621708aa40c5e4
Savagecoder77 <savagecoder77@gmail.com>
2019-04-21 10:22:53 -0700

    remove notes and moved to sound.py in marvin-modules

--- marvin.py
+++ marvin.py
@@ -6,9 +6,3 @@ sys.path.insert(0, path.join('marvin-env', 'lib', 'site-packages'))
 
 
 gui.mainloop()
-
-# Sounds, Terminal Control
-# linux sounds amixer sset 'Master' 50% change percent
-# mac sounds  osascript -e "set Volume 0" Lowest
-# mac sounds  osascript -e "set Volume 3.5" Middle
-# mac sounds  osascript -e "set Volume 7" Max
