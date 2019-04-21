8eccf447af8fa2b043bec1baa518101fb940adef
Savagecoder77 <savagecoder77@gmail.com>
2019-04-21 10:49:06 -0700

    get all input

--- marvin-modules/executables/speaking.py
+++ marvin-modules/executables/speaking.py
@@ -4,4 +4,4 @@ from os import path
 sys.path.insert(0, path.join(path.dirname(path.abspath(__file__)), 'marvin-modules')) # make env marvin-modules folder in path for pip installed libraries
 import speak as marvin_speak
 
-marvin_speak.say(str(sys.argv[1])) # speak
+marvin_speak.say(str(sys.argv[1:])) # speak
