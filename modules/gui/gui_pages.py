5d3b97959a39514690c3ac2d792a96a3ad6d984d
Savagecoder77 <savagecoder77@gmail.com>
2019-04-23 16:34:29 -0700

    add background color

--- modules/gui/gui_pages.py
+++ modules/gui/gui_pages.py
@@ -13,36 +13,36 @@ LARGE_FONT = ("Verdana", 30)
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent, bg='#191919')
         label = tk.Label(self, text='Marvin Start Page', font=LARGE_FONT, fg='#e2e2e2', bg='#191919')
         label.pack(pady=10, padx=10)
 
-        button1 = ttk.Button(self, text='Settings',
-                            command=lambda: controller.show_frame(SettingsPage))
+        button1 = tk.Button(self, text='Settings',
+                            command=lambda: controller.show_frame(SettingsPage), highlightbackground='#191919')
         button1.pack()
 
-        button2 = ttk.Button(self, text='Start',
-                            command=lambda: controller.show_frame(MainPage))
+        button2 = tk.Button(self, text='Start',
+                            command=lambda: controller.show_frame(MainPage), highlightbackground='#191919')
         button2.pack()
 
 class SettingsPage(tk.Frame):
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Settings", font=LARGE_FONT)
+        tk.Frame.__init__(self, parent, bg='#191919')
+        label = tk.Label(self, text="Settings", font=LARGE_FONT, fg='#e2e2e2', bg='#191919')
         label.pack(pady=10, padx=10)
 
-        button1 = ttk.Button(self, text='Back to Start Page',
-                            command=lambda: controller.show_frame(StartPage))
+        button1 = tk.Button(self, text='Back to Start Page',
+                            command=lambda: controller.show_frame(StartPage), highlightbackground='#191919')
         button1.pack()
 
 class MainPage(tk.Frame):
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Page One", font=LARGE_FONT)
+        tk.Frame.__init__(self, parent, bg='#191919')
+        label = tk.Label(self, text="Page One", font=LARGE_FONT, fg='#e2e2e2', bg='#191919')
         label.pack(pady=10, padx=10)
 
-        button1 = ttk.Button(self, text='Back to Start Page',
-                            command=lambda: controller.show_frame(StartPage))
+        button1 = tk.Button(self, text='Back to Start Page',
+                            command=lambda: controller.show_frame(StartPage), highlightbackground='#191919')
         button1.pack()
