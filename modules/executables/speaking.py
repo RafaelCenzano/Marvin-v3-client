a192d67e68bcae457cb8ef11002de537de579ba5
Savagecoder77 <savagecoder77@gmail.com>
2019-04-25 13:33:14 -0700

    change to pyttsx speak

--- modules/executables/speaking.py
+++ modules/executables/speaking.py
@@ -2,6 +2,6 @@
 import sys
 from os import path
 sys.path.insert(0, path.join(path.dirname(path.abspath(__file__)), 'marvin-modules')) # make env marvin-modules folder in path for pip installed libraries
-import speak as marvin_speak
+from modules.pyttsx_speak import pyttsx_speak
 
-marvin_speak.say(str(sys.argv[1:])) # speak
+pyttsx_speak(str(sys.argv[1:]))
