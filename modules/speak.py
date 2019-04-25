ad4ab3bc43db84dafe346dd2cd4c1f5e60c5f2ec
Savagecoder77 <savagecoder77@gmail.com>
2019-04-25 13:32:50 -0700

    use tts

--- modules/speak.py
+++ modules/speak.py
@@ -1,5 +1,11 @@
-from modules.pyttsx_speak import pyttsx_speak
 from modules.gtts_speak import gtts_speak
+from modules.network import internet_on
+from sys import executable as python_path
+from os import system as terminal
 
-def speak(tts):
-    pass
+def say(tts):
+    check_connection = internet_on()
+    if check_connection == True and speak_type == 1:
+    	gtts_speak(tts)
+    elif check_connection == False and speak_type == 2:
+    	terminal(f'{python_path} executables/speaking.py {tts}')
