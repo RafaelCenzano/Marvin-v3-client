21691b60d325a2489d7f8b3112fb9dfbb5ff1826
Savagecoder77 <savagecoder77@gmail.com>
2019-04-27 23:22:37 -0700

    import tests

--- marvin.py
+++ marvin.py
@@ -5,6 +5,7 @@ from os import system as terminal
 from platform import system as platform
 # make env sitepackages folder in path for pip installed libraries
 sys.path.insert(0, path.join('marvin-env', 'lib', 'site-packages'))
+from tests.test_manager import test_manager
 
 if platform() == 'Darwin':  # How Mac OS X is identified by Python
     terminal('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
