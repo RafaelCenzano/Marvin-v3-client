ffdb09dded9e30fd57a7bbc3c5325213b2d69927
Savagecoder77 <savagecoder77@gmail.com>
2019-04-27 23:32:16 -0700

    rework setup.py and try to fix imports and installs

--- setup.py
+++ setup.py
@@ -1,60 +1,68 @@
 # Imports
-from os import path, mkdir, listdir, remove
+from os import path, mkdir, listdir, remove, getcwd
 from os import system as terminal
 from json import dump
 from platform import system
 from shutil import rmtree, move
 import sys
 
+try:
+    confirm = input('\n\n\n\nConfirm that you want to run steup.py. y/n? ').lower()
+except:
+    print('\nNot python 3\n')
+    sys.exit(0)
+if confirm == 'y' or confirm == 'yes' or confirm == 'true':
+    pass
+else:
+    print('\nsetup.py cancled by user\n')
+    sys.exit(0)
+
+
 # Run code if file directly called and app.py, requirements.txt, and
 # __init__.py in marvin-env exists to make many checks that setup.py
 # called in right directory
-if __name__ == '__main__' and path.isfile('app.py') and path.isfile(
-        'requirements.txt') and path.isfile(path.join('marvin-env', 'lib', 'site-packages', '__init__.py')):
+if __name__ == '__main__' and path.isfile('marvin.py') and path.isfile(
+        'requirements.txt') and path.isfile(path.join('marvinenv', 'lib', 'sitepackages', '__init__.py')):
 
     try:
-        terminal(
-            'pip install tqdm==4.28.1 -t ' +
-            path.join(
-                'marvin-env',
-                'lib',
-                'site-packages') +
-            ' --upgrade')
+        if platform() == 'Darwin' and path.isfile('/usr/local/Homebrew/bin/brew') == False:
+            print('MAC os detected\nHomebrew not found\nMarvin virtual assistant needs homebrew to install portaudio\n')
+            allow_brew_install = input('Allow the setup.py to install Homebrew using Ruby and cURL? y/n? ').lower()
+
+            if allow_brew_install == 'y' or allow_brew_install == 'yes' or allow_brew_install == 'true':
+                print('\n\nInstalling Homebrew from https://raw.githubusercontent.com/Homebrew/install/master/install\n\n')
+                terminal('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
+
+                allow_portaudio_install = input('Allow the setup.py to install portaudio using Homebrew? y/n? ').lower()
+                if allow_portaudio_install == 'y' or allow_portaudio_install == 'yes' or allow_portaudio_install == 'true':
+                    print('\n\nInstalling portaudio from Homebrew\n\n')
+                    terminal('brew install portaudio')
+            else:
+                print('There will be an error later when installing pyaudio. Read more about port audio here: http://www.portaudio.com/')
+
     except Exception as e:
-        print(f'There was an error: {e}')
+        print(f'There was an error when working with Homebrew: {e}')
 
     try:
-        # make env sitepackages folder in path for pip installed libraries
-        sys.path.insert(0, path.join('marvin-env', 'lib', 'site-packages'))
-        from tqdm import tqdm
+        marvin_env = path.join('marvinenv','lib','sitepackages')
+        terminal(f'pip install tqdm==4.31.1 --target=={marvin_env} --upgrade')
     except Exception as e:
-        print(f'There was an error: {e}')
+        print(f'There was an error when installing tqdm: {e}')
 
     try:
-        # Make marvin-data directory to store any json and data needed to run
-        # marvin
-        mkdir('marvin-data')
-        print('\nmarvin-data folder created for marvin data\n')
-
-        # If folder exsists prompt user that this can potentially interfere
-        # with marvin code
-    except FileExistsError:
-        print('Error marvin-data folder exists already might interfere with marvin data')
+        from marvinenv.lib.sitepackages.tqdm import tqdm
+    except Exception as e:
+        print(f'There was an error import tqdm: {e}')
 
     try:
         # install required libraries into marvin-env
         print('\nInstalling needed libraries\n')
-        terminal(
-            'pip install -r requirements.txt -t ' +
-            path.join(
-                'marvin-env',
-                'lib',
-                'site-packages') +
-            ' --upgrade')
+        marvin_env = path.join('marvinenv','lib','sitepackages')
+        terminal(f'pip install  -r requirements.txt -t {marvin_env} --upgrade')
 
         # dependencies couldn't be installed
     except Exception as e:
-        print(f'We ran into a problem\nPlease report this issue {e} \nFiles couldn\'t be installed properly')
+        print(f'We ran into a problem\nPlease report this issue: {e} \nFiles couldn\'t be installed properly')
 
         # make list full of uneeded folders created from pip installs
 
@@ -82,7 +90,7 @@ if __name__ == '__main__' and path.isfile('app.py') and path.isfile(
         for dirs in tqdm(uneeded_dirs):
 
             # create path to folder to delete
-            folder = path.join('marvin-env', 'lib', 'site-packages', dirs)
+            folder = path.join('marvinenv', 'lib', 'sitepackages', dirs)
 
             # remove all files in the directory
             for files in listdir(folder):
@@ -104,24 +112,32 @@ if __name__ == '__main__' and path.isfile('app.py') and path.isfile(
 
         # files and folders couln't be deleted
     except Exception as e:
-        print(f'This error occured when trying to remove uneeded folders to save space {e}. If this a problem please report the error to the github repository')
+        print(f'This error occured when trying to remove uneeded folders to save space: {e}. If this a problem please report the error to the github repository')
 
-    source = path.join('marvin-env', 'lib', 'site-packages', 'bin')
-    new_bin_folder = path.join('marvin-env', 'bin')
+    source = path.join('marvinenv', 'lib', 'sitepackages', 'bin')
+    new_bin_folder = path.join('marvinenv', 'bin')
 
     try:
         files = listdir(source)
 
         for f in tqdm(files):
             not_wanted_bin = path.join(
-                'marvin-env', 'lib', 'site-packages', 'bin', f)
+                'marvinenv', 'lib', 'sitepackages', 'bin', f)
             move(not_wanted_bin, new_bin_folder)
 
         rmtree(source)
         print('deleted ' + source + ' folder')
 
     except Exception as e:
-        print(f'This error occured when trying to remove uneeded folders to save space {e}. If this a problem please report the error to the github repository')
+        print(f'This error occured when trying to remove uneeded folders to save space: {e}. If this a problem please report the error to the github repository')
+
+    try:
+        absolute_path = getcwd()
+        path_file = open(path.join('marvinenv','path.py'), 'w+')
+        path_file.write(f'marvin_path = {absolute_path}')
+        path_file.close()
+    except Exception as e:
+        print(f'This error occured when trying to save data: {e}. If this a problem please report the error to the github repository')
 
     # file ran from outside marvin folder which causes path problems for file
     # creation
