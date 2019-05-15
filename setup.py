e8278811d08e18602a71683d5b0ca314b1c14c66
Savagecoder77 <savagecoder77@gmail.com>
2019-05-14 23:04:55 -0700

    change some code

--- setup.py
+++ setup.py
@@ -16,8 +16,6 @@ else:
     print('\nsetup.py cancled by user\n')
     sys.exit(0)
 
-tqdm_here = False
-
 # Run code if file directly called and app.py, requirements.txt, and
 # __init__.py in marvin-env exists to make many checks that setup.py
 # called in right directory
@@ -26,7 +24,7 @@ if __name__ == '__main__' and path.isfile('marvin.py') and path.isfile(
 
     try:
         if platform() == 'Darwin' and path.isfile('/usr/local/Homebrew/bin/brew') == False:
-            print('MAC os detected\nHomebrew not found\nMarvin virtual assistant needs homebrew to install portaudio\n')
+            print('\nMAC os detected\nHomebrew not found\nMarvin virtual assistant needs homebrew to install portaudio\n')
             allow_brew_install = input('Allow the setup.py to install Homebrew using Ruby and cURL? y/n? ').lower()
 
             if allow_brew_install == 'y' or allow_brew_install == 'yes' or allow_brew_install == 'true':
@@ -41,7 +39,7 @@ if __name__ == '__main__' and path.isfile('marvin.py') and path.isfile(
                 print('There will be an error later when installing pyaudio. Read more about port audio here: http://www.portaudio.com/')
 
         else:
-            allow_portaudio_install = input('Allow the setup.py to install portaudio using Homebrew? y/n? ').lower()
+            allow_portaudio_install = input('\nAllow the setup.py to install portaudio using Homebrew? y/n? ').lower()
             if allow_portaudio_install == 'y' or allow_portaudio_install == 'yes' or allow_portaudio_install == 'true':
                 print('\n\nInstalling portaudio from Homebrew\n\n')
                 terminal('brew install portaudio')
@@ -52,10 +50,23 @@ if __name__ == '__main__' and path.isfile('marvin.py') and path.isfile(
         print(f'There was an error when working with Homebrew: {e}')
 
     try:
-        # install required libraries into marvin-env
-        print('\nInstalling needed libraries\n')
-        marvin_env = path.join('marvinenv','lib','sitepackages')
-        terminal(f'pip3 install  -r requirements.txt -t {marvin_env}')
+        absolute_path = getcwd()
+        path_file = open(path.join('marvinenv','path.py'), 'w+')
+        path_file.write(f'marvin_path = \'{absolute_path}\'')
+        path_file.close()
+    except Exception as e:
+        print(f'This error occured when trying to save data: {e}. If this a problem please report the error to the github repository')
+
+    try:
+        install_requirements = input('\nAllow setup.py to install all required libraries? librariescan be seen in requirements.txt. y/n? ').lower()
+        if install_requirements == 'y' or install_requirements == 'yes' or install_requirements == 'true'
+            # install required libraries into marvin-env
+            print('\nInstalling needed libraries\n')
+            marvin_env = path.join('marvinenv','lib','sitepackages')
+            terminal(f'pip3 install  -r requirements.txt -t {marvin_env} --upgrade')
+        else:
+            print('Program will not work without needed libraries')
+            sys.exit(0)
 
         # dependencies couldn't be installed
     except Exception as e:
@@ -77,7 +88,8 @@ if __name__ == '__main__' and path.isfile('marvin.py') and path.isfile(
         'requests-2.20.0.dist-info',
         'six-1.11.0.dist-info',
         'PyAudio-0.2.11.dist-info',
-        'bs4-0.0.1.dist-info']
+        'bs4-0.0.1.dist-info',
+        'urllib3-1.24.2.dist-info']
 
     try:
         # for all uneeded folders delete them
@@ -125,13 +137,6 @@ if __name__ == '__main__' and path.isfile('marvin.py') and path.isfile(
     except Exception as e:
         print(f'This error occured when trying to remove uneeded folders to save space: {e}. If this a problem please report the error to the github repository')
     '''
-    try:
-        absolute_path = getcwd()
-        path_file = open(path.join('marvinenv','path.py'), 'w+')
-        path_file.write(f'marvin_path = \'{absolute_path}\'')
-        path_file.close()
-    except Exception as e:
-        print(f'This error occured when trying to save data: {e}. If this a problem please report the error to the github repository')
 
     # file ran from outside marvin folder which causes path problems for file
     # creation
