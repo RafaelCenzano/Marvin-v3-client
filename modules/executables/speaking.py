ddbb637d5212e646b92e66f96428a08c33ed733e
Savagecoder77 <savagecoder77@gmail.com>
2019-08-18 22:47:19 -0700

    remove uneeded imports and old test

--- modules/executables/speaking.py
+++ modules/executables/speaking.py
@@ -1,7 +1,5 @@
 # Speak file
 import sys
-from os import path
-sys.path.insert(0, path.join(path.dirname(path.abspath(__file__)), 'marvin-modules')) # make env marvin-modules folder in path for pip installed libraries
 from modules.pyttsx_speak import pyttsx_speak
 
 pyttsx_speak(str(sys.argv[1:]))
